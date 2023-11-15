6ee77a2e02210ec16344d1586bf30d73657db0d9
KKRG92 <kevinclint.rg@gmail.com>
2023-11-15 15:48:58 +0100

    new modif

--- main.sh
+++ main.sh
@@ -1,6 +1,13 @@
 echo "Bonjour git
 
 a=2
+<<<<<<< HEAD
 echo $a puis $(($a+1)) ca fait 465 non? 
+=======
+
+echo "nouvelle modif de Paname"
+
+echo $a puis $(($a+1)) ca fait 3
+>>>>>>> 0cb6b7a (rajout d'1 ligne echo)
 ls
 exit 1
